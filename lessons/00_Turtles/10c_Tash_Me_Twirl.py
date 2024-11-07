7398efa2423d843e2c3d65bed5a4e52a79b39391
Laith10000 <laithayhamskaf@gmail.com>
2024-11-07 23:49:25 +0000

    hola soi dora V2

--- lessons/00_Turtles/10c_Tash_Me_Twirl.py
+++ lessons/00_Turtles/10c_Tash_Me_Twirl.py
@@ -6,6 +6,42 @@ so the moustache will twirl when you click on it.
 
 Hint: See 08a_More Turtle Programs, section 'Click on the Turtle'
 """
+import turtle as turtle
+
+t = turtle.Turtle()
+screen = turtle.Screen()
+screen.setup(width=600, height=600, startx=0, starty=0)
+screen.bgcolor('white')
+
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+# Set up the screen
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+# Create a turtle and set its shape to the custom GIF
+t = turtle.Turtle()
+
+set_turtle_image(t, "pikachu.gif")
 
 ... # Your code here
+def screen_clicked(x, y):
+    """Print the x and y coordinates of the screen when clicked.
+    and make the turtle move to the clicked location."""
 
+    print('You pressed: x=' + str(x) + ', y=' + str(y))
+
+    t.goto(x, y)
+  
+screen.onclick(screen_clicked)
+
+turtle.done()
